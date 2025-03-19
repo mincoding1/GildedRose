4eb2f766bed0c0f9750514a67ca1ae9e98d7ff00
mincoding <74457149+mincoding1@users.noreply.github.com>
2025-03-19 19:30:23 +0900

    Update gilded_rose_test.cpp

--- cpp/gilded_rose_test.cpp
+++ cpp/gilded_rose_test.cpp
@@ -2,9 +2,14 @@
 #include "gilded_rose.h"
 
 TEST(GildedRoseTest, Foo) {
+    //Arrange
     vector<Item> items;
     items.push_back(Item("Foo", 0, 0));
     GildedRose app(items);
+
+    //Act
     app.updateQuality();
-    EXPECT_EQ("fixme", app.items[0].name);
+
+    //Arrange
+    EXPECT_EQ("fixme", app.items[0].name); //코드 이해 후, 수정 필요
 }
