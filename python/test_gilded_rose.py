70aa1c6fd06756748f5103d16cae55f725ba9938
mincoding <74457149+mincoding1@users.noreply.github.com>
2025-06-01 13:19:35 +0900

    Update test_gilded_rose.py

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,9 +1,9 @@
 import pytest
 from gilded_rose import Item, GildedRose
 
-class test_gilded_rose:
-    def test_foo(self):
-        items = [Item('foo', 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        assert items[0].name == 'fixme'
+def test_foo():
+    items = [Item('foo', 0, 0)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].name == 'fixme'
+
